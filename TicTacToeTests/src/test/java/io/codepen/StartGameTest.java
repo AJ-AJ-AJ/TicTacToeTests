a89bfddbeddd694b2edd92b8d54767d4e08dd1ac
AJ-AJ-AJ <ajimenez@scmail.spelman.edu>
2019-03-24 16:25:42 -0400

    Sent number to game and pressed play to start game

--- TicTacToeTests/src/test/java/io/codepen/StartGameTest.java
+++ TicTacToeTests/src/test/java/io/codepen/StartGameTest.java
@@ -1,7 +1,10 @@
 package io.codepen;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.Test;
 
 public class StartGameTest {
@@ -16,7 +19,12 @@ public class StartGameTest {
 			String url = "https://codepen.io/jshlfts32/full/bjambP/";
 			driver.get(url);
 			//Send number to game
+			driver.switchTo().frame(driver.findElement(By.id("result")));
+			WebElement inputField = driver.findElement(By.id("number"));
+			inputField.sendKeys("3");
 			//Press play to start game
+			WebElement playButton = driver.findElement(By.id("start"));
+			playButton.click();
 			
 		}
 }
