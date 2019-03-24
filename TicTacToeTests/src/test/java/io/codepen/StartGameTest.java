11ab0e56c21445a9de86ca2f6a2d188b58936df1
AJ-AJ-AJ <ajimenez@scmail.spelman.edu>
2019-03-24 17:42:05 -0400

    Added verification to confirm table is visible once game is started

--- TicTacToeTests/src/test/java/io/codepen/StartGameTest.java
+++ TicTacToeTests/src/test/java/io/codepen/StartGameTest.java
@@ -4,7 +4,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class StartGameTest {
@@ -18,13 +18,24 @@ public class StartGameTest {
 			//Navigate to codepen
 			String url = "https://codepen.io/jshlfts32/full/bjambP/";
 			driver.get(url);
+			
 			//Send number to game
 			driver.switchTo().frame(driver.findElement(By.id("result")));
 			WebElement inputField = driver.findElement(By.id("number"));
-			inputField.sendKeys("3");
+			inputField.sendKeys("2");
+			
 			//Press play to start game
 			WebElement playButton = driver.findElement(By.id("start"));
 			playButton.click();
 			
+			//Verifications
+			
+			//table is visible
+			//If the id "1" is not found, the table isn't visible
+			WebElement tableRow = driver.findElement(By.id("1"));
+			//Assert.assertTrue(tableRow.isDisplayed(), "Table is not visible");
+			
+			//close browser
+			driver.quit();
 		}
 }
