b79bf7329be70cb6b8bb97b6aef82adeef6695de
AJ-AJ-AJ <ajimenez@scmail.spelman.edu>
2019-03-24 18:55:08 -0400

    Player has automatically entered X in first table box

---
+++ TicTacToeTests/src/test/java/io/codepen/FirstMoveTest.java
@@ -0,0 +1,44 @@
+package io.codepen;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class FirstMoveTest {
+
+	@Test
+	public void MakeFirstMove() {
+		// Create driver
+		System.setProperty("webdriver.chrome.driver", "src/main/resources/chromedriver");
+		WebDriver driver = new ChromeDriver();
+
+		// Navigate to codepen
+		String url = "https://codepen.io/jshlfts32/full/bjambP/";
+		driver.get(url);
+
+		// Send number to game
+		driver.switchTo().frame(driver.findElement(By.id("result")));
+		WebElement inputField = driver.findElement(By.id("number"));
+		inputField.sendKeys("3");
+
+		// Press play to start game
+		WebElement playButton = driver.findElement(By.id("start"));
+		playButton.click();
+		
+		//Click to make first move
+		WebElement firstTableSpace = driver.findElement(By.id("0"));
+		firstTableSpace.click();
+
+		// Verifications
+
+		// table is visible
+		// If the id "1" is not found, the table isn't visible
+		WebElement tableRow = driver.findElement(By.id("1"));
+		// Assert.assertTrue(tableRow.isDisplayed(), "Table is not visible");
+
+		// close browser
+		//driver.quit();
+	}
+}
